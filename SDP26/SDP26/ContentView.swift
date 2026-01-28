9cc80bb147b2fcd1b892c537d5a92a0a182f2697
José Luis <joseluiscorrallopez@Joses-MacBook-Air.local>
2026-01-28 18:16:46 +0100

    App creation

---
+++ SDP26/SDP26/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SDP26
+//
+//  Created by José Luis Corral López on 28/1/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SDP26/SDP26/SDP26App.swift
@@ -0,0 +1,17 @@
+//
+//  SDP26App.swift
+//  SDP26
+//
+//  Created by José Luis Corral López on 28/1/26.
+//
+
+import SwiftUI
+
+@main
+struct SDP26App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
